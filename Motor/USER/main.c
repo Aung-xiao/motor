e20e337b2f7a65d02dfb3d508ba4e8d2c412ba48
Aung-xiao <2557160927@qq.com>
2022-08-29 14:50:29 +0800

    Revert "3.0"
    This reverts commit b6a94d8f2f2df7176db8745289e86fdfea3ae0ac.

--- Motor/USER/main.c
+++ Motor/USER/main.c
@@ -3,17 +3,8 @@
 #include "sys.h"
 #include "includes.h"
 
-float Velocity_A=10;
-int main(void)
-{	
-		NVIC_PriorityGroupConfig(NVIC_PriorityGroup_4);
-	 	CAN1_Init();																		// CAN1
-		M3508_Motor_Init();															// M3508µç»ú	
-		while(1)
-		{
-			PID_incremental_PID_calculation(&M3508_CHASSIS_MOTOR_PID_RPM[0], M3508_CHASSIS_MOTOR_REAL_INFO[0].RPM ,Velocity_A);
-			M3508_CHASSIS_MOTOR_REAL_INFO[0].TARGET_CURRENT = M3508_CHASSIS_MOTOR_PID_RPM[0].output;
-			chassis_m3508_send_motor_currents_can1();			
-		}
-}
+ int main(void)
+ {	
+
+ }
 
